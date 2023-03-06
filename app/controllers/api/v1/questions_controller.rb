8aab378b8ddaf65fc121e874f86fcca89a89acf8
AJ-Kulundu <jameskulundu@gmail.com>
2023-03-06 16:55:28 +0300

    Creating a response api without db

---
+++ app/controllers/api/v1/questions_controller.rb
@@ -0,0 +1,108 @@
+require 'dotenv'
+require 'cosine_similarity'
+
+Dotenv.load
+
+class Api::V1::QuestionsController < ApplicationController
+    def index
+       embeddings =  load_embeddings("embeddings.csv")
+       similar = order_document_sections_by_similarity("What is microsoft doing in cloud computing?", embeddings)
+       render json: similar
+    end
+
+    def create
+        embeddings =  load_embeddings("embeddings.csv")
+        question  = question_params[:question]
+        answer,context = answer_query_with_context(question, embeddings)
+
+        render json: {answer: answer, context: context.join(" ")}
+    end
+
+    private
+    def question_params
+        params.require(:question).permit(:question)
+    end
+
+    def get_embeddings(question)
+        response = HTTParty.post('https://api.openai.com/v1/embeddings',{
+            headers: {
+                "Content-Type" => "application/json",
+                "Authorization" =>"Bearer #{ENV['OPENAI_API_KEY']}"
+            },
+            body: {
+                "model" => "text-embedding-ada-002",
+                "input"=> question
+            }.to_json
+        })
+         embedding = JSON.parse(response.body)
+        
+        return embedding["data"][0]["embedding"]
+    end
+
+    def load_embeddings(fname)
+        csv_data =  CSV.read(fname, headers: true)
+        document_embeddings = {}
+        csv_data.each do |row|
+            hash_key = [JSON.parse(row["index"]), row["token"]]
+            document_embeddings[hash_key] = JSON.parse(row["embeddings"])
+        end
+        return document_embeddings
+    end
+
+    def vector_similarity(a, b)
+        # Ensure embeddings are of the same length
+        unless a.length == b.length
+            raise "Embeddings are not of the same length"
+        end
+
+        return cosine_similarity(a, b)
+    end
+
+    def order_document_sections_by_similarity(question, context)
+        question_embedding = get_embeddings(question)
+        documents = []
+        context.each do |key, value|
+        documents <<[vector_similarity(question_embedding, value), key]
+        end
+        similar_documents = documents.sort{ |a,b| b[0] <=> a[0]}
+        return similar_documents
+    end
+
+    def construct_prompt(question,embeddings)
+       relevant_documents = order_document_sections_by_similarity(question, embeddings)
+       context = []
+       count = 0 
+       relevant_documents.each do |document|
+          if count < 2
+          context << document[1][1]
+          count += 1
+          else
+            break
+          end
+       end
+       header = "Answer the following question based on the context provided and if the answer isn\'t provided within the context say \" I don\'t know\". Please keep your answers to 4 sentences maximum, and speak in complete sentences. Stopspeaking once your point is made. \n \n"
+       prompt = header+ "Context: \n\n" + context.join(" ") + "\n\nQ: "+question+ "\n\n A: "
+       return prompt, context
+    end
+
+    def answer_query_with_context(question,embeddings)
+         prompt,context =  construct_prompt(question,embeddings);
+         response = HTTParty.post('https://api.openai.com/v1/completions',{
+            headers: {
+                "Content-Type" => "application/json",
+                "Authorization" =>"Bearer #{ENV['OPENAI_API_KEY']}"
+            },
+            body: {
+                model:"text-davinci-003",
+                prompt: prompt,
+                max_tokens: 150,
+                temperature: 0,
+
+            }.to_json})
+
+            data = JSON.parse(response.body)
+
+            return data["choices"][0]["text"],context
+    end
+
+end
